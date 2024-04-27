755c8e49084acd203833d6598c46761767f945c9
TeodoroMG <teodoromatarritagomez@gmail.com>
2024-04-27 05:45:44 -0600

    crea alura radio

---
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -0,0 +1,48 @@
+package com.aluracursos.radioalura.principal;
+
+import com.aluracurso.radioalura.modelos.Cancion;
+import com.aluracurso.radioalura.modelos.MisFavoritos;
+import com.aluracurso.radioalura.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Gabriela Aguiar");
+        miPodcast.setTitulo("Cafe.Tech");
+
+        // cancion
+
+        for (int i = 0; i < 100; i++) {
+          miCancion.meGusta();
+        }
+
+        for (int i = 0; i < 2000; i++) {
+        miCancion.reproduce();
+        }
+
+
+       // Podcast
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
+        }
+
+
+
+
+
+        System.out.println(" Total reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println(" Total de Me Gusta: " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(miPodcast);
+        favoritos.adicione(miCancion);
+    }
+}
